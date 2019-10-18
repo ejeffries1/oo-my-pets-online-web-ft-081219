a0d95b992bacaa5b8662e8263b06bd78b3f413be
Evan Jeffries <ejeffries14@gmail.com>
2019-10-18 02:11:36 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,38 @@
 class Owner
-  # code goes here
+  attr_accessor
+  attr_reader :species, :name
+  
+  @@all = []
+  def initialize(species = "human", name)
+    @species = species
+    @name = name
+    @@all << self
+  end
+  
+  def species
+    @species
+  end
+  
+  def say_species
+    return "I am a human."
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.count
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  
+  def cats(owner)
+    Cat.all.map do |own|
+      @@all << own.owner
+    end
+    @@all
+  end
 end
